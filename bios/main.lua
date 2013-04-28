3090ca81180187f0da29a9f13a9dbe7461ee8a8d
Stiepen22 <stiepen22@gmx.de>
2013-04-28 18:48:19 +0200

    Misc pre-dinner commit

---
+++ bios/main.lua
@@ -0,0 +1,31 @@
+local requirecache = setmetatable({}, {__mode = "w"})
+
+function require( lib )
+    if requirecache[lib] then return requirecache[lib] end
+    local lib_ = lib
+    local env = setmetatable({}, {__index = _ENV})
+    if (not nativefs.exists("/bios/lib/"..lib)) and nativefs.exists("/bios/lib/"..lib..".lua") then
+        lib = lib..".lua"
+    end
+    local fn, err = loadfile("/bios/lib/"..lib)
+    if not fn then 
+        term.write("Lib error:")
+        term.setCursorPos(1, 2)
+        term.write(err)
+        return nil
+    end
+    setfenv(fn, env)
+    requirecache[lib_] = env
+    local s, m = pcall(fn)
+    if not s then 
+        term.write("Lib error:")
+        term.setCursorPos(1, 2)
+        term.write(m)
+        requirecache[lib_] = nil
+        return nil
+    end
+    return env
+end
+
+partsys = require('part');
+partsys.initpartitions
